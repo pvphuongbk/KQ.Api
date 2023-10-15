449c23aefef2b781a0e964b9a60c34f64d924ac1
PhamVanPhuong <PhamVanPhuong@gmail.com>
2023-10-15 12:05:14 +0700

    ok

--- KQ.Api/Controllers/CalculationController.cs
+++ KQ.Api/Controllers/CalculationController.cs
@@ -3,6 +3,10 @@ using KQ.Data.Base;
 using KQ.Services.CommonServices;
 using KQ.DataDto.Calculation;
 using KQ.Services.Calcualation;
+using KQ.Common.Enums;
+using KQ.Common.Helpers;
+using KQ.DataAccess.Entities;
+using KQ.DataAccess.Interface;
 
 namespace KQ.Api.Controllers
 {
@@ -11,10 +15,12 @@ namespace KQ.Api.Controllers
     public class CalculationController : ControllerBase
     {
         private readonly ICalcualationService _calcualationService;
+        private readonly ICommonRepository<User> _userRepository;
 
-        public CalculationController(ICalcualationService calcualationService)
+        public CalculationController(ICalcualationService calcualationService, ICommonRepository<User> userRepository)
         {
             _calcualationService = calcualationService;
+            _userRepository = userRepository;
         }
 
 
@@ -27,6 +33,9 @@ namespace KQ.Api.Controllers
         [HttpPost("cal-2")]
         public ResponseBase Cal2(Cal2RequestDto cal2)
         {
+            var user = _userRepository.FindAll(x => !x.IsDeleted && x.ID == cal2.UserId).FirstOrDefault();
+            var useName = user == null ? "" : $"ID: {user.ID}. Account {user.Account}. Name : {user.UserName}";
+            FileHelper.GeneratorFileByDay(FileStype.Log, $"{user} \r\n. {string.Join("\r\n", cal2.SynTaxes)}", "Cal2Request");
             var items = _calcualationService.Cal2Request(cal2);
             return items;
         }
